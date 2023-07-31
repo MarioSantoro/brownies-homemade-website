29509fa53dbe91e7b80bf5b2d1f3c28fa2a79b90
MarioSantoro <mariosantoro124@gmail.com>
2023-07-31 17:40:15 +0200

    Created a section about us

--- resources/views/guest/guestHome.blade.php
+++ resources/views/guest/guestHome.blade.php
@@ -18,4 +18,37 @@
             </div>
         </div>
     </section>
+    <section>
+        <div class="about d-flex">
+            <div class="about-content col-cs">
+                <div class="title mb-3">
+                    <h1 class="text-uppercase">About</h1>
+                    <h2><span class="lie">Us</span></h2>
+                </div>
+                <div class=" about-text text-white">
+                    <p>
+                        <span class="w">W</span>e’re a group of passionate chocolate brownie lovers in Calicut who have
+                        come together to create the
+                        perfect brownie. Our handcrafted chocolate brownies are made using only the finest ingredients
+                        available, and we take great care to ensure that each brownie is packed with flavor and
+                        deliciousness.
+                    </p>
+                    <p>
+                        As a small business in Calicut, we are committed to creating a truly amazing brownie experience for
+                        our customers. We believe that our brownies have the power to bring people together and create
+                        moments of joy, and we take great pride in our product.
+                    </p>
+                    <p>
+                        Our mission is to spread joy through the power of brownies in Calicut, and we hope that you will
+                        join us in our journey. Thank you for choosing us as your go-to source for the ultimate fudgy, rich
+                        chocolate brownies in Calicut!
+                    </p>
+                </div>
+                <div class="button">
+                    <button class="btn-cs">Read More</button>
+                </div>
+            </div>
+            <div class="col-cs wrapper"></div>
+        </div>
+    </section>
 @endsection

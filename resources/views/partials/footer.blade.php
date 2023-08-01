d8309e72fe94159fe6290c543896efac9b763298
MarioSantoro <mariosantoro124@gmail.com>
2023-08-01 14:27:49 +0200

    added general footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,14 @@
 <footer>
-    <h1>Footer</h1>
+    <div class="container text-white mt-4">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between align-items-center">
+                <p>© 2023 BrashBrownie | Powered by obliv</p>
+                <div class="icons ">
+                    <i class="fa-brands fa-square-facebook fa-xl"></i>
+                    <i class="fa-brands fa-twitter fa-xl  ms-3"></i>
+                    <i class="fa-brands fa-square-instagram fa-xl  ms-3"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>

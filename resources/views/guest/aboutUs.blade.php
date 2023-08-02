f09bd38f693ec9e81112a7d48e764b94ca394585
MarioSantoro <mariosantoro124@gmail.com>
2023-08-02 10:09:30 +0200

    complete page about

--- resources/views/guest/aboutUs.blade.php
+++ resources/views/guest/aboutUs.blade.php
@@ -1,19 +1,52 @@
 @extends('layout.app')
 
 @section('main-content')
-    <div class="jumbotron">
-        <div class="title">
-            <h1 class="text-uppercase">Cashew</h1>
-            <h2>Brownie</h2>
+    <section>
+        <div class="jumbotron first">
+            <div class="title">
+                <h1 class="text-uppercase">About</h1>
+                <h2>Us</h2>
+            </div>
+            <div class="content">
+                <div class="column first">
+                    <p><span class="change">W</span>elcome to our chocolate brownie business! We are a passionate group of
+                        individuals who have
+                        come
+                        together with a shared love for all things sweet and chocolatey. Our journey started with a simple
+                        desire to create the perfect fudge brownies one that was rich, fudgy, and packed with flavor.
+                    </p>
+                    <p>After countless experiments in the kitchen, we finally hit upon the winning recipe, and from there,
+                        our
+                        business was born. Today, we are proud to offer our delicious brownies to customers far and wide,
+                        sharing our love of baking with everyone who comes our way.</p>
+                </div>
+            </div>
         </div>
-        <div class="spacer"></div>
-        <div class="elementor">
-            <p>Meet the famous chocolate <br><span class="lie">Brownies</span> in Calicut
-            </p>
+    </section>
+    <section>
+        <div class="jumbotron second">
+            <div class="content d-flex">
+                <div class="column"></div>
+                <div class="column second">
+                    <p>What sets us apart from other brownie makers is our commitment to quality. We only use the finest
+                        ingredients, sourcing the best chocolate, butter, and sugar we can find to create our signature
+                        chocolate brownie recipe. Each fudge brownie is handcrafted with care, ensuring that every bite is a
+                        delight for the senses.
+                    </p>
+                    <p>At our core, we are a small business with big dreams. We believe that chocolate brownies have the
+                        power to bring people together and create moments of joy and connection. That’s why we take pride in
+                        every order that comes our way, and why we are always looking for ways to innovate and improve our
+                        chocolate and fudge brownies.</p>
+                    <p>
+                        Whether you’re looking for a sweet treat for yourself or a gift for someone special, we hope you’ll
+                        consider choosing our chocolate brownies or fudge brownies. With every order, you’re not just
+                        supporting our business you’re becoming part of our story.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="button">
-            <button class="btn-cs">Shop Now</button>
-        </div>
+    </section>
+    <div class="logo">
     </div>
 @endsection
 @vite('resources/scss/about.scss')
